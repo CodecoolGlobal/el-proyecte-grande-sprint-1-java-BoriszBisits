f8f75e1d01fa49910c33a2c816c482cfd11caa1e
Borisz Bisits <118134911+BoriszBisits@users.noreply.github.com>
2023-08-28 13:38:03 +0200

    add HelloWorld

--- circles-backend/src/main/java/com/codecool/circles/controller/ProjectController.java
+++ circles-backend/src/main/java/com/codecool/circles/controller/ProjectController.java
@@ -1,4 +1,15 @@
 package com.codecool.circles.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class ProjectController {
+    @GetMapping(value = "hello")
+    public String getMessege(){
+        return "hello";
+    }
 }
