bba2c7d2850cfa8a2a640a1d1451d430c623ee65
Borisz Bisits <118134911+BoriszBisits@users.noreply.github.com>
2023-09-13 13:52:59 +0200

    add member to task

---
+++ circles-backend/src/main/java/com/codecool/circles/service/dao/MemberDao.java
@@ -0,0 +1,11 @@
+package com.codecool.circles.service.dao;
+
+import com.codecool.circles.model.Member;
+import com.codecool.circles.repositories.MemberRepository;
+
+import java.util.List;
+
+public interface MemberDao {
+    public void saveMember(Member member);
+public void saveMembers(List<Member> memberList);
+}
---
+++ circles-backend/src/main/java/com/codecool/circles/service/dao/MemberDaoImpl.java
@@ -0,0 +1,29 @@
+package com.codecool.circles.service.dao;
+
+import com.codecool.circles.model.Member;
+import com.codecool.circles.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Access;
+import java.util.List;
+
+@Repository
+public class MemberDaoImpl implements MemberDao{
+    private MemberRepository memberRepository;
+    @Autowired
+    public MemberDaoImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void saveMember(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public void saveMembers(List<Member> memberList) {
+
+        memberRepository.saveAll(memberList);
+    }
+}
