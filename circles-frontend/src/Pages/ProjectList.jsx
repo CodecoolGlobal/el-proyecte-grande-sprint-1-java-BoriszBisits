54cda539c93243523237dc1ac339fd871ddad015
Tihamer Antal <118159451+Antal01@users.noreply.github.com>
2023-09-14 16:34:19 +0200

    Project.css finish

--- circles-frontend/src/Pages/ProjectList.jsx
+++ circles-frontend/src/Pages/ProjectList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../App.css";
+
 
 function ProjectList() {
     const navigate = useNavigate();
