a7d7ad8c96858b12a11cdd2555fbe9b3cb5de010
Balazs Furedi <115081757+Balazs60@users.noreply.github.com>
2023-11-09 13:12:52 +0100

    Fix delete for projects, tasks and subtasks

--- circles-backend/src/main/java/com/codecool/circles/service/dao/MainPageDaoImpl.java
+++ circles-backend/src/main/java/com/codecool/circles/service/dao/MainPageDaoImpl.java
@@ -6,6 +6,9 @@ import com.codecool.circles.model.Task;
 import com.codecool.circles.model.storage.Storage;
 import com.codecool.circles.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -33,6 +36,18 @@ public class MainPageDaoImpl implements MainPageDao {
     public void addNewProject(Project project) {
         System.out.println("new project: " + project);
         projectRepository.save(project);
+    }
+
+    @Override
+    public ResponseEntity<String> deleteProjectById(Long projectId) {
+        try {
+            projectRepository.deleteById(projectId);
+            return new ResponseEntity<>("Project deleted successfully", HttpStatus.OK);
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity<>("Project not found", HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error occurred while deleting the project", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
